6f8371c935ba1c170ab9a4682d8312fc81a22e48
Joe <guozhijie_2006@126.com>
2017-08-03 10:15:47 +0800

    [Feature] First commit

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 4 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#4 SMP PREEMPT Sat Jul 29 21:45:38 CST 2017"
+#define LINUX_COMPILE_BY "ldd"
+#define LINUX_COMPILE_HOST "ldd-ubuntu"
+#define LINUX_COMPILER "gcc version 4.4.1 (Sourcery G++ Lite 2009q3-67) "
